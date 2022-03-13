ae3247f4f991a210dd6e28388b40907466b57391
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2022-03-13 14:37:51 -0500

    add delay between frames

--- graphics/src/bin.rs
+++ graphics/src/bin.rs
@@ -6,7 +6,15 @@ use graphics::{
     Renderer,
     Canvas2d,
 };
-use std::sync::Arc;
+use std::{
+    sync::Arc,
+    time::{
+        SystemTime,
+        Instant,
+        Duration,
+    },
+    thread::sleep,
+};
 use anyhow::*;
 use winit_main::{
     EventLoopHandle,
@@ -22,33 +30,37 @@ use tracing_subscriber::{
 };
 
 
-fn draw_frame(mut canvas: Canvas2d) {
-    canvas
-        .with_clip_min_x(0.1)
-        .with_clip_min_y(0.2)
-        .with_clip_max_x(0.7)
-        .with_clip_max_y(0.6)
-        .draw_solid();
-    /*
-    canvas
-        .with_clip_min_x(0.5)
-        .with_translate([0.25, 0.25])
-        .draw_solid();*/
-    /*
-    canvas
-        .with_translate([0.25, 0.5])
-        .draw_solid();
-    canvas
-        .with_scale([0.2, 0.2])
-        .with_translate([0.4, 0.4])
-        .with_color([0xff, 0, 0xff, 0xff / 2])
-        .draw_solid();*/
+struct Graphics {
+    start_time: SystemTime,
+}
+
+impl Graphics {
+    fn draw_frame(&mut self, mut canvas: Canvas2d) {
+        let elapsed = self.start_time.elapsed().unwrap().as_millis() as f32 / 1000.0;
+        let scale = elapsed.sin() * 0.4 + 0.6;
+        trace!(%scale);
+        let translate = (1.0 - scale) / 2.0;
+        canvas
+            .with_clip_min_x(0.1)
+            .with_clip_min_y(0.2)
+            .with_clip_max_x(0.7)
+            .with_clip_max_y(0.6)
+            .with_scale([scale, scale])
+            .with_translate([translate, translate])
+            .draw_solid();
+    }
 }
 
 async fn window_main(event_loop: EventLoopHandle, mut events: EventReceiver) -> Result<()> {
     let window = event_loop.create_window(Default::default()).await?;
     let window = Arc::new(window);
     let mut renderer = Renderer::new(Arc::clone(&window)).await?;
+    let mut graphics = Graphics {
+        start_time: SystemTime::now(),
+    };
+
+    let frames_per_second = 60;
+    let frame_delay = Duration::from_secs(1) / frames_per_second;
 
     loop {
         let event = events.recv().await;
@@ -63,13 +75,26 @@ async fn window_main(event_loop: EventLoopHandle, mut events: EventReceiver) -> 
                 _ => (),
             },
             Event::MainEventsCleared => {
+                let before_frame = Instant::now();
+
+                // draw frame
                 trace!("drawing frame");
                 let result = renderer.draw_frame(|canvas| {
-                    draw_frame(canvas);
+                    graphics.draw_frame(canvas);
                 });
                 if let Err(e) = result {
                     error!(error=%e, "draw_frame error");
                 }
+
+                // wait
+                let after_frame = Instant::now();
+                let delay = (before_frame + frame_delay)
+                    .checked_duration_since(after_frame);
+                if let Some(delay) = delay {
+                    sleep(delay);
+                }
+
+                // request redraw
                 window.request_redraw();
             },
             _ => (),
