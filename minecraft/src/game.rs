70b07b3083086acba3b153bb9e059b26e4711ee7
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2022-09-04 15:14:27 -0500

    delete commented out code

--- minecraft/src/game.rs
+++ minecraft/src/game.rs
@@ -1,43 +1,6 @@
 
 use crate::{
     jar_assets::JarReader,
-    /*
-    ui::{
-        ui_block_items_struct,
-        UiBlock,
-        UiBlockSetWidth,
-        UiBlockSetHeight,
-        center_block::{
-            UiHCenterBlock,
-            UiVCenterBlock,
-        },
-        layer_block::UiLayerBlock,
-        stable_unscaled_size_block::{
-            UiStableUnscaledWidthBlock,
-            UiStableUnscaledHeightBlock,
-        },
-        stack_block::UiVStackBlock,
-        text_block::{UiTextBlock, UiTextBlockConfig},
-        tile_9_block::{
-            UiTile9Block,
-            UiTile9BlockConfig,
-            Tile9Images,
-            LoadTile9ImagesConfig,
-        },
-        tile_block::{
-            UiTileBlock,
-            UiTileBlockConfig,
-        },
-        margin_block::{
-            UiHMarginBlock,
-            UiHMarginBlockConfig,
-            UiVMarginBlock,
-            UiVMarginBlockConfig,
-        },
-        mc::{
-            title::UiMcTitleBlock,
-        },
-    },*/
     ui2::*,
 };
 use graphics::{
@@ -155,49 +118,6 @@ struct GuiState {
 
     splash_text: McSplashTextGuiBlock,
 }
-/*
-type MainMenu =
-    UiLayerBlock<(
-        UiTileBlock, // background
-        UiHMarginBlock< // corner text
-            UiVMarginBlock<
-                UiLayerBlock<(
-                    UiTextBlock, // version
-                    UiTextBlock, // uncopyright
-                )>,
-            >,
-        >,
-        UiVCenterBlock< // center column
-            UiVStackBlock<(
-                UiHCenterBlock< // title block
-                    UiStableUnscaledWidthBlock<
-                        UiStableUnscaledHeightBlock<
-                            UiMcTitleBlock,
-                        >,
-                    >,
-                >,
-                UiHCenterBlock< // buttons
-                    UiStableUnscaledWidthBlock<
-                        UiVStackBlock<(
-                            Button, // singleplayer
-                            Button, // multiplayer
-                            Button, // mods
-                            Button, // options
-                        )>,
-                    >,
-                >,
-            )>,
-        >,
-    )>;
-
-type Button =
-    UiStableUnscaledHeightBlock<
-        UiLayerBlock<(
-            UiTile9Block,
-            UiTextBlock,
-        )>
-    >;
-*/
 
 impl GuiState {
     fn gui<'a>(&'a mut self) -> impl GuiBlock<'a, DimParentSets, DimParentSets> {
@@ -360,22 +280,6 @@ impl Game {
         let lang = jar.read_properties("lang/en_US.lang").await?;
 
         let font = renderer.load_font_437(jar.read("font/default.png").await?)?;
-        
-        /*
-        let splash_text = UiText::new(
-            &renderer,
-            UiTextConfig {
-                text: "Splash text!".into(),
-                font,
-                font_size: 32.0,
-                color: Rgba::yellow(),
-                h_align: HAlign::Center,
-                v_align: VAlign::Center,
-            },
-            None,
-            size.scale,
-        );
-        */
 
         let raw_title_pixel_texture = jar.read_image_part("terrain.png", [16, 0], [16, 16]).await?;
 
@@ -400,12 +304,6 @@ impl Game {
             px_right: 2,
         }.load(&renderer);
 
-        // version_text: TextGuiBlock,
-        // copyright_text: TextGuiBlock,
-        // singleplayer_button_text: TextGuiBlock,
-        // multiplayer_button_text: TextGuiBlock,
-        // mods_button_text: TextGuiBlock,
-        // options_button_text: TextGuiBlock,
         let version_text = TextGuiBlock::new(
             vec![TextGuiBlockSpan {
                 text: "Not Minecraft Beta 1.0.2".into(),
@@ -474,235 +372,13 @@ impl Game {
             false,
         );
 
-/*
-        fn create_button(
-            width: f32,
-            scale: f32,
-            text: String,
-            renderer: &Renderer,
-            images: Tile9Images,
-            font: FontId,
-        ) -> Button {
-            UiStableUnscaledHeightBlock::new(
-                40.0,
-                |size, scale| UiLayerBlock::new(
-                    |size, scale| (
-                        UiTile9Block::new(
-                            UiTile9BlockConfig {
-                                images,
-                                size_unscaled_untiled: Extent2::new(200.0, 20.0) * 2.0,
-                                frac_top: 2.0 / 20.0,
-                                frac_bottom: 3.0 / 20.0,
-                                frac_left: 2.0 / 200.0,
-                                frac_right: 2.0 / 200.0,
-                            },
-                            size,
-                            scale,
-                        ),
-                        UiTextBlock::new(
-                            &renderer,
-                            UiTextBlockConfig {
-                                text,
-                                font,
-                                font_size: 16.0,
-                                color: hex_color(0xE0E0E0FF),
-                                h_align: HAlign::Center,
-                                v_align: VAlign::Center,
-                                wrap: false,
-                            },
-                            size,
-                            scale,
-                        ),
-                    ),
-                    size,
-                    scale,
-                ),
-                width,
-                scale,
-            )
-        }*/
-
         let bg_image = renderer.load_image(jar.read("gui/background.png").await?)?;
-        /*
-        let ui = UiLayerBlock::new(
-            |size, scale| (
-                UiTileBlock::new(
-                    UiTileBlockConfig {
-                        image: bg_image,
-                        size_unscaled_untiled: [64.0; 2].into(),
-                        color: [0.25, 0.25, 0.25, 1.0].into(),
-                    },
-                    size,
-                    scale,
-                ),
-                UiHMarginBlock::new(
-                    UiHMarginBlockConfig {
-                        margin_left: 4.0,
-                        margin_right: 4.0,
-                    },
-                    |size, scale| UiVMarginBlock::new(
-                        UiVMarginBlockConfig {
-                            margin_top: 4.0,
-                            margin_bottom: 4.0,
-                        },
-                        |size, scale| UiLayerBlock::new(
-                            |size, scale| (
-                                UiTextBlock::new(
-                                    &renderer,
-                                    UiTextBlockConfig {
-                                        text: "Not Minecraft Beta 1.0.2".into(),
-                                        font,
-                                        font_size: 16.0,
-                                        color: hex_color(0x505050FF),
-                                        h_align: HAlign::Left,
-                                        v_align: VAlign::Top,
-                                        wrap: true,
-                                    },
-                                    size,
-                                    scale,
-                                ),
-                                UiTextBlock::new(
-                                    &renderer,
-                                    UiTextBlockConfig {
-                                        text: "Everything in the universe is in the public domain.".into(),
-                                        font,
-                                        font_size: 16.0,
-                                        color: Rgba::white(),
-                                        h_align: HAlign::Right,
-                                        v_align: VAlign::Bottom,
-                                        wrap: true,
-                                    },
-                                    size,
-                                    scale,
-                                ),
-                            ),
-                            size,
-                            scale,
-                        ),
-                        size,
-                        scale,
-                    ),
-                    size,
-                    scale,
-                ),
-                UiVCenterBlock::new(
-                    |scale| UiVStackBlock::new(
-                        25.0,
-                        |width, scale| (
-                            UiHCenterBlock::new(
-                                |scale| UiStableUnscaledWidthBlock::new(
-                                    600.0,
-                                    |size, scale| UiStableUnscaledHeightBlock::new(
-                                        300.0,
-                                        |size, scale| UiMcTitleBlock::new(
-                                            &renderer,
-                                            &mut rng,
-                                            raw_title_pixel_texture,
-                                            size,
-                                            scale,
-                                        ),
-                                        size.w,
-                                        scale,
-                                    ),
-                                    width,
-                                    scale,
-                                ),
-                                size,
-                                scale,
-                            ),
-                            UiHCenterBlock::new(
-                                |scale| UiStableUnscaledWidthBlock::new(
-                                    400.0,
-                                    |size, scale| UiVStackBlock::new(
-                                        8.0,
-                                        |width, scale| (
-                                            create_button(
-                                                width,
-                                                scale,
-                                                lang["menu.singleplayer"].clone(),
-                                                &renderer,
-                                                button_images.clone(),
-                                                font,
-                                            ),
-                                            create_button(
-                                                width,
-                                                scale,
-                                                lang["menu.multiplayer"].clone(),
-                                                &renderer,
-                                                button_images.clone(),
-                                                font,
-                                            ),
-                                            create_button(
-                                                width,
-                                                scale,
-                                                lang["menu.mods"].clone(),
-                                                &renderer,
-                                                button_images.clone(),
-                                                font,
-                                            ),
-                                            create_button(
-                                                width,
-                                                scale,
-                                                lang["menu.options"].clone(),
-                                                &renderer,
-                                                button_images.clone(),
-                                                font,
-                                            ),
-                                        ),
-                                        size.w,
-                                        scale,
-                                    ),
-                                    width,
-                                    scale,
-                                ),
-                                size,
-                                scale,
-                            ),
-                        ),
-                        size.w,
-                        scale,
-                    ),
-                    size,
-                    scale,
-                ),
-            ),
-            size,
-            scale,
-        );*/
-
+       
         let title_block = McTitleGuiBlock::new(
             &renderer,
             &mut rng,
             raw_title_pixel_texture,
         );
-        /*
-        let splash_text = TextGuiBlock::new(
-            vec![TextGuiBlockSpan {
-                text: "Information wants to be free!".into(),
-                font,
-                color: Rgba::yellow(),
-            }],
-            32.0,
-            HAlign::Left,
-            VAlign::Top,
-            true,
-        );
-        let splash_size = Cosine::new(1.0 / 2.0);
-*/
-        /*
-         let splash_text = UiText::new(
-            &renderer,
-            UiTextConfig {
-                text: "Splash text!".into(),
-                font,
-                font_size: 32.0,
-                color: Rgba::yellow(),
-                h_align: HAlign::Center,
-                v_align: VAlign::Center,
-            },
-            None,
-            size.scale,
-        );*/
 
         let splash_text = McSplashTextGuiBlock::new(
             &renderer,
@@ -751,22 +427,9 @@ impl Game {
             debug_dot: None,
         })
     }
-    /*
-    fn gui<'a>(&'a self) -> impl GuiBlock<'a, DimParentSets, DimParentSets> {
-        tile_image_gui_block(
-            &self.bg_image,
-            [64.0; 2],
-        )
-    }
-    */
+
     pub async fn draw<'a>(&mut self, elapsed: f32) -> Result<()> {
         trace!(%elapsed, "updating");
-        /*
-        self.splash_size.add_to_input(elapsed);
-        for pos in &mut self.title_pixel_positions {
-            pos.z = f32::min(0.0, pos.z + 75.0 * elapsed);
-        }
-        */
 
         self.gui_state.title_block.update(elapsed);
         self.gui_state.splash_text.update(elapsed);
@@ -774,9 +437,6 @@ impl Game {
         trace!("drawing");
 
         let mut frame = FrameContent::new();
-        //let mut canvas = frame.canvas();
-
-        //self.ui.draw(canvas.reborrow());
 
         struct DrawGuiVisitorTarget<'r, 'a, 'b> {
             renderer: &'r Renderer,
@@ -807,21 +467,6 @@ impl Game {
             }));
         }
 
-        /*
-        {
-            let mut canvas = canvas.reborrow()
-                .translate(Vec2 {
-                    x: self.size.size.w / 4.0 * 3.0,
-                    y: self.size.size.h / 16.0 * 5.0,
-                })
-                .scale([
-                    self.splash_size.get().abs() / 16.0 + 1.0; 2
-                ])
-                .rotate(f32::to_radians(22.5));
-            self.splash_text.draw(canvas.reborrow());
-        }
-        */
-
         self.renderer.draw_frame(&frame)
     }
 
@@ -831,8 +476,6 @@ impl Game {
         self.renderer.resize(size);
         self.size = size.map(|n| n as f32);
 
-        //self.ui.set_width(&self.renderer, self.size.w);
-        //self.ui.set_height(&self.renderer, self.size.h);
         
         Ok(())
     }
@@ -842,8 +485,6 @@ impl Game {
 
         self.scale = scale;
 
-        //self.ui.set_scale(&self.renderer, self.scale);
-        
         Ok(())
     }
 
