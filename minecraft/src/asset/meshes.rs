bd6dd707bf6ab6c72866d12cd05fcd482d9537c9
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2023-07-02 19:15:12 -0500

    ninety degrees

--- minecraft/src/asset/meshes.rs
+++ minecraft/src/asset/meshes.rs
@@ -22,9 +22,9 @@ pub fn block_item_mesh(tex_index: usize) -> MeshData {
     let shade_c = 0x39 as f32 / 0x8f as f32;
     mesh_buf
         .add_quad(&Quad {
-            pos_start: [0.0, 1.0, 0.0].into(),
-            pos_ext_1: [0.0, 0.0, 1.0].into(),
-            pos_ext_2: [1.0, 0.0, 0.0].into(),
+            pos_start: [1.0, 1.0, 0.0].into(),
+            pos_ext_1: [-1.0, 0.0, 0.0].into(),
+            pos_ext_2: [0.0, 0.0, 1.0].into(),
             tex_start: 0.0.into(),
             tex_extent: 1.0.into(),
             vert_colors: [[shade_a, shade_a, shade_a, 1.0].into(); 4],
