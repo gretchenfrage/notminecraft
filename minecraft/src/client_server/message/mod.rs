dba5dc5b5cef6119c5563ebe787d20da4daebc9a
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2023-08-28 21:07:23 -0500

    delete commented out code

--- minecraft/src/client_server/message/mod.rs
+++ minecraft/src/client_server/message/mod.rs
@@ -63,37 +63,3 @@ message_enum!(down DownMessage {
         edit: Edit,
     }
 });
-
-/*
-/// Message sent from client to server.
-#[derive(Debug, GameBinschema)]
-pub enum UpMessage {
-    SetTileBlock(UpMessageSetTileBlock),
-}
-
-#[derive(Debug, GameBinschema)]
-pub struct UpMessageSetTileBlock {
-    pub gtc: Vec3<i64>,
-    pub bid: RawBlockId,
-}
-
-/// Message sent from server to client.
-#[derive(Debug, GameBinschema)]
-pub enum DownMessage {
-    LoadChunk(DownMessageLoadChunk),
-    ApplyEdit(DownMessageApplyEdit),
-}
-
-#[derive(Debug, GameBinschema)]
-pub struct DownMessageLoadChunk {
-    pub cc: Vec3<i64>,
-    pub ci: usize,
-    pub chunk_tile_blocks: ChunkBlocks,
-}
-
-#[derive(Debug, GameBinschema)]
-pub struct DownMessageApplyEdit {
-    pub ci: usize,
-    pub edit: Edit,
-}
-*/
